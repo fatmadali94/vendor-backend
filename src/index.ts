77f571e14c94d7ffac39ad295b679dae04536471
kasra1993 <kasramahsouli@gmail.com>
2024-09-25 18:23:59 +0330

    changes to how we handle cors

--- src/index.ts
+++ src/index.ts
@@ -24,33 +24,33 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 // app.use(helmet());
 app.use(compression());
 
-app.use(
-  cors({
-    // origin: [
-    //   "https://demo-1.chiliscript.de",
-    //   "http://localhost:5173",
-    //   "https://localhost:5173",
-    //   "localhost:5173",
-    // ],
-    origin: "*",
-    // credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     // origin: [
+//     //   "https://demo-1.chiliscript.de",
+//     //   "http://localhost:5173",
+//     //   "https://localhost:5173",
+//     //   "localhost:5173",
+//     // ],
+//     origin: "*",
+//     // credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
-app.options("*", cors()); // Handle preflight requests globally
+// app.options("*", cors()); // Handle preflight requests globally
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://demo-1.chiliscript.de"); // Set allowed origin manually
-//   res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH"
-//   ); // Set allowed methods
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Set allowed headers
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  ); // Set allowed methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Set allowed headers
+  next();
+});
 
 app.use((req, res, next) => {
   res.cookie("mycookie", "value", {
