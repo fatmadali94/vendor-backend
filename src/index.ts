638ae97b9c1d5c67f702fa3d5fb4e54818b891c9
kasra1993 <kasramahsouli@gmail.com>
2024-09-25 14:18:08 +0330

    some minor changes

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import http from "http";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
-import cors, { CorsOptions } from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import crypto from "crypto";
+import helmet from "helmet";
+import cors from "cors";
 
 // import dotenv from "dotenv";
 
@@ -16,36 +17,20 @@ dotenv.config();
 const app = express();
 
 app.use(express.static("public"));
-app.use(compression());
 app.use(cookieParser());
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(helmet());
 
-const corsOptions: CorsOptions = {
-  origin: function (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean) => void
-  ) {
-    const allowedOrigins = [
-      "https://demo-1.chiliscript.de", // Your live frontend
-      "http://localhost:3000", // Your local frontend
-    ];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // Ensure cookies and credentials are allowed
-  allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Allow necessary HTTP methods
-};
-
-app.use(cors(corsOptions));
-
-// Handle preflight (OPTIONS) requests explicitly
-app.options("*", cors(corsOptions));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://demo-1.chiliscript.de/"], // Your React app's local address
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use((req, res, next) => {
   res.cookie("mycookie", "value", {
@@ -60,11 +45,12 @@ const server = http.createServer(app);
 const port = 3004;
 server.listen(port, () => {
   console.log("server running on http://localhost:3004/");
-  console.log("working great");
 });
 
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGO_URL! as string);
+mongoose.connect(process.env.MONGO_URL! as string, {
+  maxPoolSize: 10,
+});
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
-app.use("/server", router());
+app.use("/server", compression(), router());
